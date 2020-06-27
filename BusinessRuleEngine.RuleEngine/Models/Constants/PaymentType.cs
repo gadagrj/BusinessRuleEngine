8e5b46e830d10f2b9766305bb80e28087133e94b
Ravi <rjgadag@gmail.com>
2020-06-27 23:13:25 +0530

    Added Logic for the first iteration.

---
+++ BusinessRuleEngine.RuleEngine/Models/Constants/PaymentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.RuleEngine.Models.Constants
+{
+    public enum PaymentType
+    {
+        PHYSICAL_PRODUCT = 1 , 
+        BOOK = 2, 
+        MEMBERSHIP = 3, 
+        VIDEO = 4 
+    }
+}
---
+++ BusinessRuleEngine.RuleEngine/Models/Constants/ProductTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRuleEngine.RuleEngine.Models.Constants
+{
+    public enum ProductTypes
+    {
+        BOOK = 1, 
+        VIDEO = 2, 
+        Physical = 3,
+        
+    }
+}
